a03db693c5f1cad55a9226c40a5aec564202765f
Deniz Dağlı <denizdagli@Deniz-MacBook-Pro.local>
2023-11-11 12:41:52 +0300

    partials olusturuldu

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,14 +1,26 @@
-exports.getAboutPage = (req, res) => {
-    res.render('about');
-  };
+const e = require("express");
+const { get } = require("mongoose");
 
 exports.getIndexPage = (req, res) => {
   res.render('index');
 };
+exports.getShopPage = (req, res) => {
+  res.render('shop');
+  };
+
+exports.getAboutPage = (req, res) => {
+    res.render('about');
+  };
+
+exports.getServicesPage = (req, res) => {
+    res.render('services');
+  };
+
+exports.getBlogPage = (req, res) => {
+    res.render('blog');
+  }
+
 exports.getCartPage = (req, res) => {
   res.render('cart');
 };
 
-exports.getIndexPage = (req, res) => {
-res.render('shop');
-};
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const pageControllers = require('../controllers/pageControllers');
 
-
-router.get("/about", pageControllers.getAboutPage);
 router.get("/index", pageControllers.getIndexPage);
-router.get("/cart", pageControllers.getAboutPage);
-router.get("/shop", pageControllers.getIndexPage);
+router.get("/shop", pageControllers.getShopPage);
+router.get("/about", pageControllers.getAboutPage);
+router.get("/services", pageControllers.getServicesPage);
+router.get("/blog", pageControllers.getBlogPage);
+router.get("/cart", pageControllers.getCartPage);
+
 module.exports = router;  
