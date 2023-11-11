2a5d62b37d6ccca432d29d3b14fb92775f6a8bd8
Deniz Dağlı <denizdagli@Deniz-MacBook-Pro.local>
2023-11-11 17:12:22 +0300

    hakkımızda sayfası güncellendi

--- app.js
+++ app.js
@@ -9,18 +9,11 @@ const port = process.env.PORT || 3000;
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
-app.set('views', './views');
+
 //MIDDLEWARES
 app.use(express.json());
-
-
-
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/img', express.static(__dirname + 'public/img'));
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/",pageRoute);
 
 // app.use(methodOverride('_method', {
