b489541f8a1d90aa7b1134453a6215855dac1b04
Acelya777 <acelyayildirim2020@gmail.com>
2023-11-15 14:09:09 +0300

    basket tablosuna transfer işlemi

--- controllers/dbconfig.js
+++ controllers/dbconfig.js
@@ -2,16 +2,22 @@ const DynamoDBClient = require('@aws-sdk/client-dynamodb').DynamoDBClient;
 const DynamoDBDocumentClient = require('@aws-sdk/lib-dynamodb').DynamoDBDocumentClient;
 
 
+
 const client = new DynamoDBClient({
     region: 'us-east-1',
     credentials: {
-        accessKeyId: 'AKIARJ7HFT53M7L2H37J',
-        secretAccessKey: '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',
+        accessKeyId: "AKIARJ7HFT53M7L2H37J",
+        secretAccessKey: "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",
     },
 });
 
 const db = DynamoDBDocumentClient.from(client);
 
-const Table = 'Carpet'
+const Table1 = 'Carpet';
+const Table2 = 'Basket';
 
-module.exports = {db,Table};
+module.exports = {
+    db,
+    Table1,
+    Table2
+};
